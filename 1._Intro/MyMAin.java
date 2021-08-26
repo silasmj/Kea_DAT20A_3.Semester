1f04334e85bde6d341448201608ce2599297ad5a
silasmjensen <silasmj@icloud.com>
2021-08-26 12:32:19 +0200

    ll

---
+++ 1._Intro/MyMAin.java
@@ -0,0 +1,13 @@
+public class MyMAin {
+    public static void main(String[] args) {
+
+        Systemos.out.println("Hej med dig!");
+        Systemos.out.println(5);
+        Systemos.out.println(d);
+    }
+
+    /* public static <T> void out(T out){
+        System.out.println(out);
+    } */
+
+}
---
+++ 1._Intro/Systemos.java
@@ -0,0 +1,16 @@
+public class Systemos {
+
+    public static class out{
+        public static void println(String s) {
+            System.out.println(s);
+        }
+        public static void println(int x) {
+            System.out.println(x);
+        }
+
+        public static void println(double d) {
+            System.out.println(d);
+        }
+
+    }
+}
