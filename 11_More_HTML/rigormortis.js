41565bd5016387b85cd0f08d6c03238d28064f8d
silasmj <74252789+silasmj@users.noreply.github.com>
2021-10-26 11:47:09 +0200

    search

--- 11_More_HTML/rigormortis.js
+++ 11_More_HTML/rigormortis.js
@@ -1,11 +1,44 @@
- fetch("https://rickandmortyapi.com/api/character")
+const characterGalleryDiv = document.getElementById("character-gallery")
+let characters;
+let filteredCharacters;
+
+fetch("https://rickandmortyapi.com/api/character")
         .then(response => response.json())
         .then(result => {
-            result.results.map(character => createCharacterCard(character));
+            characters = result.results;
+            filteredCharacters = characters;
+            characters.map(character => createCharacterCard(character));
         });
 
 
  function createCharacterCard(character) {
-     console.log(character);
+     const cardElement = document.createElement("div")
+
+     cardElement.innerHTML = `
+        <p>${escapeHTML(character.name)}</p>
+        <img src="${escapeHTML(character.image)}"></img>
+     `;
+
+     characterGalleryDiv.appendChild(cardElement)
+ }
+
+ document.getElementById("search").addEventListener("click", searchCharacters); // Uden parantes - Det skal ikke kaldes, når siden bliver loadet. Uden parantes, giver man kun referencen. Så når eventet bliver triggered, begynder den at søge.
+document.getElementById("name-search").addEventListener("input", handleSearchName)
+
+function handleSearchName(event) {
+    characterGalleryDiv.innerHTML = "";
+    filteredCharacters.filter(character => character.name.toLowerCase().includes(event.target.value.toLowerCase())).map(createCharacterCard);
+}
+
+function searchCharacters() {
+     const selectedStatus = document.getElementById("status-dropdown").value;
+     characterGalleryDiv.innerHTML = "";
+     if (selectedStatus === "All") {
+         filteredCharacters = characters;
+         characters.map(createCharacterCard)
+     }else{
+         filteredCharacters = characters.filter(character => character.status === selectedStatus);
+         filteredCharacters.map(createCharacterCard);
+     }
  }
 
