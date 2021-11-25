335066b7ad82f16237ebea0352bf649650dd1d53
silasmj <74252789+silasmj@users.noreply.github.com>
2021-11-25 11:47:02 +0100

    sadsad

--- 12_Prices_stores/client/store/store.js
+++ 12_Prices_stores/client/store/store.js
@@ -1,5 +1,25 @@
-fetch("http://localhost:8080/prices")
-    .then(response => response.json())
-    .then(result => {
-        console.log(result)
-    })
+const queryString = window.location.search;
+const urlParams = URLSearchParams(queryString);
+const storeId = parseInt(urlParams.get("storeId"));
+
+const pricesDiv = document.getElementById("prices-display-case")
+
+const prices = [
+    {
+        id: 1, price: 84.00, size: "10px", color: "green"
+    },
+    {
+        id: 2, price: 8.40, size: "44px", color: "purple"
+    },
+];
+prices.map(displayPrice)
+
+function displayPrice(price) {
+    const priceTagParagraph = document.createElement("p");
+    priceTagParagraph.innerText = price.price.toFixed(2);
+    priceTagParagraph.style.position = "absolute";
+    priceTagParagraph.style.fontSize = price.size;
+    priceTagParagraph.style.color = price.color;
+
+    pricesDiv.append(priceTagParagraph);
+}
